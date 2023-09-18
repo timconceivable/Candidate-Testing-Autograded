d9e06a9f47283c0bd9c04e53d7d088ac975d7d60
timconceivable <headskape@yahoo.com>
2023-09-18 15:16:09 -0500

    part 1 complete

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
@@ -15,22 +15,41 @@ let questions;
 let correctAnswers;
 let candidateAnswers;
 
+/* let questions = [
+  "Who was the first American woman in space? ",
+"True or false: 5 kilometer == 5000 meters? "
+"(5 + 3)/2 * 10 = ? "
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? "
+"What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+"Sally Ride",
+"true",
+"40",
+"Trajectory",
+"3"
+];
+let candidateAnswers = ["", "", "", "", ""];
+ */
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+candidateName = input.question("Candidate Name: ");
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+candidateAnswer = input.question(question);
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+console.log("Your Answer:", candidateAnswer, 
+"\nCorrect Answer:", correctAnswer);
+if (candidateAnswer == correctAnswer) { console.log("\ncorrect!"); }
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -42,7 +61,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log("Hello,", candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
